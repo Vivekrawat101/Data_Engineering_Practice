482ae996bb28ede0444fdc7d3128dfa84c1abf08
Vivekrawat101 <75635136+Vivekrawat101@users.noreply.github.com>
2026-06-27 16:25:02 +0530

    added multithreading logic

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,6 +1,8 @@
 import requests
 import os
 import zipfile
+from concurrent.futures import ThreadPoolExecutor
+
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q1.zip",
@@ -10,32 +12,34 @@ download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2020_Q1.zip",
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip",
 ]
-
+def process_single_file(uri):
+    
+    
+    data = requests.get(uri)
+    
+    if data.status_code == 200 :
+    
+        tgt_path = os.path.join("downloads", uri.split('/')[-1])
+        
+        with open(tgt_path, "wb") as f:
+            f.write(data.content)
+        
+        with zipfile.ZipFile(tgt_path, 'r') as zip_ref:
+            zip_ref.extractall("downloads")
+        
+        os.remove(tgt_path)
+    
+    else:
+        print("Failed to fetch file from: " + uri)
 
 
 def main():
-    # your code here
-    #create the directory downloads if it doesn't exist
+
     os.makedirs("downloads", exist_ok=True)
-    #download the files one by one.
-    for uri in download_uris:
-        
-        data = requests.get(uri)
-        
-        if data.status_code == 200 :
-        
-            tgt_path = os.path.join("downloads", uri.split('/')[-1])
-            
-            with open(tgt_path, "wb") as f:
-                f.write(data.content)
-            
-            with zipfile.ZipFile(tgt_path, 'r') as zip_ref:
-                zip_ref.extractall("downloads")
-            
-            os.remove(tgt_path)
-        
-        else:
-            print("Failed to fetch file from: " + uri)
+    
+    
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        executor.map(process_single_file, download_uris)
             
     
 
