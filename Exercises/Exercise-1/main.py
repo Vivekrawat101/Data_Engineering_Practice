9cd5765aac4dc9e96d123c7030096e6db4610b99
Vivekrawat101 <75635136+Vivekrawat101@users.noreply.github.com>
2026-06-27 15:13:50 +0530

    Complete base requirements for Exercise 1: sequential download and unzip

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,5 +1,6 @@
 import requests
-
+import os
+import zipfile
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q1.zip",
@@ -11,11 +12,33 @@ download_uris = [
 ]
 
 
+
 def main():
     # your code here
+    #create the directory downloads if it doesn't exist
+    os.makedirs("downloads", exist_ok=True)
+    #download the files one by one.
+    for uri in download_uris:
         
+        data = requests.get(uri)
+        
+        if data.status_code == 200 :
+        
+            tgt_path = os.path.join("downloads", uri.split('/')[-1])
+            
+            with open(tgt_path, "wb") as f:
+                f.write(data.content)
+            
+            with zipfile.ZipFile(tgt_path, 'r') as zip_ref:
+                zip_ref.extractall("downloads")
+            
+            os.remove(tgt_path)
+        
+        else:
+            print("Failed to fetch file from: " + uri)
+            
     
-    pass
+
 
 
 if __name__ == "__main__":
